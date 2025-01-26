d4da7c50a804442ce03e18a61efd39e6ada2dd70
Nam Pham <pknam25@gmail.com>
2025-01-26 18:54:46 +0800

    Added usual LSP to config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {},
+        angularls = {},
+        cssls = {},
+        html = {},
+        pyright = {},
+        jdtls = {},
+        ts_ls = {},
+      },
+    },
+  },
+}
